3e5097b38b7c02590cb6a9082c9b9455117d1976
Saladin <joncolab@gmail.com>
2017-07-10 04:47:50 +0300

    0.0.4

--- src/scripts/js/script.js
+++ src/scripts/js/script.js
@@ -2,9 +2,14 @@
  * Created by Saladin on 09.07.2017.
  */
 var main = function () {
-    var sections = $('main > section');
+    var sections = $('main > section'),
+        introduction = $('main > section.introducing'),
+        header = $('header');
 
     sections.css('height', $(window).height() + 'px');
+    // introduction.css('padding-top',
+    //     header.css('height').substr(0, header.css('height').length - 2)
+    //     + 'px');
 };
 
 $(document).ready(main);

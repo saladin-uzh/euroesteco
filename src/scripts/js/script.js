630285e60fce91c116db6c93099397f89812f67a
Saladin <joncolab@gmail.com>
2017-07-10 04:32:01 +0300

    0.0.1

--- src/scripts/js/script.js
+++ src/scripts/js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by Saladin on 09.07.2017.
  */
+var main = function () {
+    var sections = $('main > section');
+
+    sections.css('height', $(window).height() + 'px');
+};
+
+$(document).ready(main);

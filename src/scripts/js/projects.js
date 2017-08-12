a6f40cd6b078f725a8cbeac29f57aeb5076213e9
Sher <max.pechko@gmail.com>
2017-08-13 00:38:54 +0300

    0.6.9

--- src/scripts/js/projects.js
+++ src/scripts/js/projects.js
@@ -1,4 +1,10 @@
 var projects = function () {
+    var galleryItem = $('.gallery-item'),
+        galleryDesc = $('.item-desc');
+
+    galleryItem.click(function () {
+        var current = $(this);
+    });
 
 };
 

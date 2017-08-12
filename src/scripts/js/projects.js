1df04f7fa44ca9477e579804c81915f23f5e0072
Sher <max.pechko@gmail.com>
2017-08-13 02:17:53 +0300

    0.7.1

--- src/scripts/js/projects.js
+++ src/scripts/js/projects.js
@@ -1,11 +1,17 @@
 var projects = function () {
     var galleryItem = $('.gallery-item'),
-        galleryDesc = $('.item-desc');
+        galleryDesc = $('.item-desc'),
+        images = $('.house-image, .portal'),
+        imageWidth = images.width(),
+        descBtn = $('.desc-btn');
+
+    images.height(imageWidth / 1.5);
 
     galleryItem.click(function () {
-        var current = $(this);
-    });
+        var current= $(this);
 
+        current.children(galleryDesc).toggleClass('active-desc');
+    });
 };
 
 $(document).ready(projects);

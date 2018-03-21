630a15c81b504a0cc2439f6809e08a861113fb27
princeShozanah <max.pechko@gmail.com>
2018-03-21 07:07:27 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import i18n from 'i18next';
 import {reactI18nextModule} from 'react-i18next';
-import Backend from 'i18next-xhr-backendi';
+import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';

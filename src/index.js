0277bec3acaadaa2fe8d848c155be30cc1f6a13d
princeShozanah <max.pechko@gmail.com>
2018-03-21 06:59:01 +0200

    0.0.2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+import i18n from 'i18next';
+import {reactI18nextModule} from 'react-i18next';
+import Backend from 'i18next-xhr-backendi';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(reactI18nextModule)
+    .init({
+        callbackLng: 'ru',
+        ns: ['translations'],
+        defaultNs: 'translations',
+        debug: true,
+        interpolation: {
+            escapeValue: false
+        },
+        react: {
+            wait: true
+        }
+    });
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 registerServiceWorker();

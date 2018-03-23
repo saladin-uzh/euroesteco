2b601dc500170c81c1090c19b3b8a3a36acab4fb
princeShozanah <max.pechko@gmail.com>
2018-03-23 08:35:38 +0200

    0.0.13

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,22 @@
-import React, {Component} from "react"
+import React from "react"
 import {I18n} from "react-i18next"
 
-export class Main extends Component {
+export class Main extends React.Component {
     render() {
         return (
             <I18n ns="translations">
                 {
                     (t) => (
                         <main className="container">
-                            <h1 className="main-caption">{t("test.font")}<br/><span className="name">Euro EstEco</span></h1>
-                            <h3 className="sub-caption">Yellow perch banjo catfish goosefish: triplefin blenny greenling scorpionfish. Redfish minnow, Black mackerel pompano dolphinfish.</h3>
+                            <h1 className="main-caption">
+                                {t("test.font")}
+                                <br/>
+                                <span className="name">Euro EstEco</span>
+                            </h1>
+                            <h3 className="sub-caption">
+                                Yellow perch banjo catfish goosefish: triplefin blenny greenling scorpionfish.
+                                Redfish minnow, Black mackerel pompano dolphinfish.
+                            </h3>
                             <img
                                 className="main-image img-background"
                                 src="../img/List_01.jpg"

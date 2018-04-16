fa2a09585e3ead52c11af62690a59bed2089bd30
Князь В'Ахуї <nem97.sv@gmail.com>
2018-04-16 17:03:21 +0300

    0.4.4

--- src/components/main/aboutUs.js
+++ src/components/main/aboutUs.js
@@ -2,16 +2,8 @@ import React from "react"
 import {I18n} from "react-i18next"
 import {backgroundImg} from "../../App"
 import {Files} from "../../App"
-import * as M from "materialize-css"
-import * as $ from "jquery"
 
 export class AboutUs extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-    // componentDidUpdate() {
-    //     M.Collapsible.init($(".collapsible"));
-    // }
     render() {
         return (
             <I18n ns="translations">
@@ -32,44 +24,3 @@ export class AboutUs extends React.Component {
         )
     }
 }
-
-const _collapsibleForSIP = (t) => (
-    <ul className="collapsible popout col l6">
-        <li>
-            <div className="collapsible-header"><i className="material-icons">av_timer</i>{t("about-us.header.first")}</div>
-            <div  className="collapsible-body">{t("about-us.body.first")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="icons">☺</i>{t("about-us.header.second")}</div>
-            <div  className="collapsible-body">{t("about-us.body.second")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="icons">☻</i>{t("about-us.header.third")}</div>
-            <div  className="collapsible-body">{t("about-us.body.third")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="icons">♥</i>{t("about-us.header.fourth")}</div>
-            <div  className="collapsible-body">{t("about-us.body.fourth")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="material-icons">account_balance</i>{t("about-us.header.fifth")}</div>
-            <div  className="collapsible-body">{t("about-us.body.fifth")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="material-icons">broken_image</i>{t("about-us.header.sixth")}</div>
-            <div  className="collapsible-body">{t("about-us.body.sixth")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="icons">♦</i>{t("about-us.header.seventh")}</div>
-            <div  className="collapsible-body">{t("about-us.body.seventh")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="material-icons">home</i>{t("about-us.header.eights")}</div>
-            <div  className="collapsible-body">{t("about-us.body.eights")}</div>
-        </li>
-        <li>
-            <div className="collapsible-header"><i className="material-icons">euro_symbol</i>{t("about-us.header.ninth")}</div>
-            <div  className="collapsible-body">{t("about-us.body.ninth")}</div>
-        </li>
-    </ul>
-);
--- src/components/technology/sip.js
+++ src/components/technology/sip.js
@@ -1,7 +1,15 @@
 import React from "react"
 import {I18n} from "react-i18next"
+import * as M from "materialize-css"
+import * as $ from "jquery"
 
 export class Sip extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidUpdate() {
+        M.Collapsible.init($(".collapsible"));
+    }
     render() {
         return (
             <I18n ns="translations">
@@ -37,6 +45,46 @@ export class Sip extends React.Component {
                                         <h5 className="flow-text col s12 grey-text text-darken-3">{t("technology.sip.text-2")}</h5>
                                     </div>
                                 </div>
+                            </div>
+                            <div className="section row">
+                                <ul className="collapsible popout col s12 l6 offset-l3">
+                                    <li>
+                                        <div className="collapsible-header"><i className="material-icons">av_timer</i>{t("about-us.header.first")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.first")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="icons">☺</i>{t("about-us.header.second")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.second")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="icons">☻</i>{t("about-us.header.third")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.third")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="icons">♥</i>{t("about-us.header.fourth")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.fourth")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="material-icons">account_balance</i>{t("about-us.header.fifth")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.fifth")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="material-icons">broken_image</i>{t("about-us.header.sixth")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.sixth")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="icons">♦</i>{t("about-us.header.seventh")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.seventh")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="material-icons">home</i>{t("about-us.header.eights")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.eights")}</div>
+                                    </li>
+                                    <li>
+                                        <div className="collapsible-header"><i className="material-icons">euro_symbol</i>{t("about-us.header.ninth")}</div>
+                                        <div  className="collapsible-body">{t("about-us.body.ninth")}</div>
+                                    </li>
+                                </ul>
                             </div>
                         </main>
                     )
